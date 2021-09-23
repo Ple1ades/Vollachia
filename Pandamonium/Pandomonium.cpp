7ddef7b44af443f3881d4c678479167b63a557b8
ple1ades <jadonjkl@gmail.com>
2021-09-22 19:14:04 -0700

    Test start

---
+++ Pandamonium/Pandomonium.cpp
@@ -0,0 +1,10 @@
+#include "../Vollachia/Vollachia.h"
+
+
+using namespace Vollachia;
+
+Renderer* mainRenderer;
+
+int init(){
+    mainRenderer = mainRenderer.init("Pandamonium", SDL_RENDERER_ACCELERATED, SDL_WINDOW_OPENGL)
+}
